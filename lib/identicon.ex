cb6ff5a62a0b250279136877b97f43f240ef315d
Claire Tran <c.tran83@gmail.com>
2018-04-20 20:55:35 +1000

    lect41

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -5,8 +5,9 @@ defmodule Identicon do
     |> pick_color
   end
 
-  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
-    %Identicon.Image{image | color: {r, g, b}}
+  def pick_color(image) do
+    %Identicon.Image{hex: hex_list} = image
+    hex_list
   end
 
   def hash_input(input) do
