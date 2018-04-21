45f1913793cde0e18d5ff851f48ea1699c9f9a01
Claire Tran <c.tran83@gmail.com>
2018-04-21 16:41:33 +1000

    Adding lecture 49

--- README.md
+++ README.md
@@ -15,6 +15,38 @@ def deps do
 end
 ```
 
+## Notes
+
+### EGD - Erlang Graphical Drawer
+
+`:egd` is no longer available in Elixir OTP, so to get around this
+
+mix.exs:
+
+```
+{:egd, github: "erlang/egd"}
+```
+
+To install dependencies:
+
+```
+mix deps.clean --all
+mix deps.get
+mix deps.compile
+```
+
+### Generate docs
+
+mix.exs:
+
+```
+{:ex_doc, "~> 0.12"}
+```
+
+```
+mix docs
+```
+
 Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
 and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
 be found at [https://hexdocs.pm/identicon](https://hexdocs.pm/identicon).
--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,4 +1,30 @@
 defmodule Identicon do
+
+  @moduledoc """
+    Udemy course on Elixir and Phoenix
+
+    Draw an identicon and save this to a PNG
+
+  ## Examples
+
+    iex> Identicon.main("asdf")
+      %Identicon.Image{color: {145, 46, 200},
+       grid: [{145, 0}, {46, 1}, {200, 2}, {46, 3}, {145, 4}, {3, 5}, {178, 6},
+        {206, 7}, {178, 8}, {3, 9}, {73, 10}, {228, 11}, {165, 12}, {228, 13},
+        {73, 14}, {65, 15}, {6, 16}, {141, 17}, {6, 18}, {65, 19}, {73, 20}, {90, 21},
+        {181, 22}, {90, 23}, {73, 24}],
+       hex: [145, 46, 200, 3, 178, 206, 73, 228, 165, 65, 6, 141, 73, 90, 181, 112],
+       pixel_map: [{{0, 0}, {50, 50}}, {{50, 0}, {100, 50}}, {{100, 0}, {150, 50}},
+        {{150, 0}, {200, 50}}, {{200, 0}, {250, 50}}, {{0, 50}, {50, 100}},
+        {{50, 50}, {100, 100}}, {{100, 50}, {150, 100}}, {{150, 50}, {200, 100}},
+        {{200, 50}, {250, 100}}, {{0, 100}, {50, 150}}, {{50, 100}, {100, 150}},
+        {{100, 100}, {150, 150}}, {{150, 100}, {200, 150}}, {{200, 100}, {250, 150}},
+        {{0, 150}, {50, 200}}, {{50, 150}, {100, 200}}, {{100, 150}, {150, 200}},
+        {{150, 150}, {200, 200}}, {{200, 150}, {250, 200}}, {{0, 200}, {50, 250}},
+        {{50, 200}, {100, 250}}, {{100, 200}, {150, 250}}, {{150, 200}, {200, 250}},
+        {{200, 200}, {250, 250}}]}
+  """
+
   def main(input) do
     input
     |> hash_input
@@ -6,16 +32,36 @@ defmodule Identicon do
     |> build_grid
     |> filter_odd_squares
     |> build_pixel_map
+    |> draw_image
+    |> save_image(input)
+  end
+
+  def save_image(image, input) do
+    File.write("#{input}.png", image)
+  end
+
+  def draw_image(%Identicon.Image{color: color, pixel_map: pixel_map}) do
+    image = :egd.create(250, 250)
+    fill = :egd.color(color)
+
+    Enum.each pixel_map, fn({start, stop}) ->
+      :egd.filledRectangle(image, start, stop, fill)
+    end
+
+    :egd.render(image)
   end
 
   def build_pixel_map(%Identicon.Image{grid: grid} = image) do
     pixel_map = Enum.map grid, fn({_code, index}) ->
       horizontal = rem(index, 5) * 50
       vertical = div(index, 5) * 50
+
       top_left = {horizontal, vertical}
       bottom_right = {horizontal + 50, vertical + 50}
+
       {top_left, bottom_right}
     end
+
     %Identicon.Image{image | pixel_map: pixel_map}
   end
 
